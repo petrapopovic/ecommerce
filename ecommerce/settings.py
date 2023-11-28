5ec072b9d5baa81224f2d754d9461eff44fcb26c
Petra Popovic <petrapopovic@ssklivno.net>
2023-11-28 12:13:01 +0100

    added global template folder

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store.apps.StoreConfig',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
